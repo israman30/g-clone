b8a8c30a57c4a3d28e6e25b19d79335cecc9c93e
Israel Manzo <israelmanzo814@gmail.com>
2020-12-16 21:00:43 -0500

    updated hardcode moc request for each request

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import { useStateValue } from '../../StateProvider'
-import useGoogleSearch from '../../useGoogleSearch'
-import './SearchPage.css'
+import React from "react";
+import { useStateValue } from "../../StateProvider";
+import useGoogleSearch from "../../useGoogleSearch";
+import Response from "../../response";
+import "./SearchPage.css";
 
 export default function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+  // LIVE API CALL
+  //  const { data } = useGoogleSearch(term)
 
-   const [{ term }, dispatch] = useStateValue()
-   const { data } = useGoogleSearch(term)
-   console.log(data)
+  const data = Response;
+
+  console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-         <h1>{term}</h1>
+        <h1>{term}</h1>
       </div>
       <div className="searchPage__reuslt"></div>
     </div>
-  )
+  );
 }
