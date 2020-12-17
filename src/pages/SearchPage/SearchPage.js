190f84e6315fd47cba75bd29acf1878e5866df39
Israel Manzo <israelmanzo814@gmail.com>
2020-12-16 21:27:45 -0500

    added search bar on search result page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -2,7 +2,9 @@ import React from "react";
 import { useStateValue } from "../../StateProvider";
 import useGoogleSearch from "../../useGoogleSearch";
 import Response from "../../response";
+import { Link } from "react-router-dom";
 import "./SearchPage.css";
+import Search from "../Search/Search";
 
 export default function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -15,7 +17,17 @@ export default function SearchPage() {
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-        <h1>{term}</h1>
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
+        </Link>
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
+          <div className="searchPage__options"></div>
+        </div>
       </div>
       <div className="searchPage__reuslt"></div>
     </div>
